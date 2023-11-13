995014b8fad84f565948c9b0c6a9e72e7b1a85ea
Shovit <shovitbhatta4@gmail.com>
2023-11-13 14:35:02 +0545

    added comment feature

---
+++ Notes_media/static/js/post.js
@@ -0,0 +1,61 @@
+const like_buttons = document.querySelectorAll(".like-unlike")
+const delete_buttons = document.querySelectorAll("#delete_post")
+like_buttons.forEach((like) =>{
+    like.addEventListener("click", () => {
+        const post = like.parentElement.parentElement
+        const like_count = post.querySelector("#like-count")
+        fetch(`/post/${post.id}`, {
+            method : "POST",
+            headers : {
+                "Source" : "like_unlike",
+                "Content-Type" : "application/json",
+                "X-CSRFToken" : post.dataset.csrf
+            },
+            body : JSON.stringify({
+                perform : like.id
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if (data.success == true){
+                if (like.id == "like"){
+                    like.id = "unlike"
+                    like.classList.remove("fa-regular")
+                    like.classList.add("fa-solid")
+                    like_count.innerText = parseInt(like_count.innerText) + 1
+                }
+                else{
+                    like.id = "like"
+                    like.classList.remove("fa-solid")
+                    like.classList.add("fa-regular")
+                    like_count.innerText = parseInt(like_count.innerText) - 1
+                }
+            }
+        })
+    })
+})
+
+delete_buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const post = button.parentElement
+        fetch(`/post/${post.id}`, {
+                method : "DELETE",
+                headers : {
+                    "Source" : "delete_post",
+                    "Content-Type" : "application/json",
+                    "X-CSRFToken" : post.dataset.csrf
+                },
+                body : JSON.stringify({
+                    perform : "delete"
+                })
+            })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if (data.success == true){
+                post.style.display = "none";
+            }
+        })
+    })
+})
--- Notes_media/urls.py
+++ Notes_media/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("<int:post_id>/edit", views.edit, name="edit")
+    path("post/<int:post_id>", views.post, name="post"),
 ]
